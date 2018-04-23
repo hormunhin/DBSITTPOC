6cf5a6102cd7569ea8e9ac6dd0f022d7bce150de
hormunhin <33797323+hormunhin@users.noreply.github.com>
2018-04-23 11:05:09 +0800

    Add files via upload

---
+++ src/main/java/com/example/DBSITTPOC/DbsittpocApplication.java
@@ -0,0 +1,12 @@
+package com.example.DBSITTPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsittpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsittpocApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/DBSITTPOC/DbsittpocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.DBSITTPOC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbsittpocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
