e38b0881721999a52e3b5f5df479e57fae1e1cca
hormunhin <33797323+hormunhin@users.noreply.github.com>
2018-04-23 11:02:00 +0800

    Add files via upload

---
+++ HKTR/HK/src/main/java/com/example/HK/HkApplication.java
@@ -0,0 +1,12 @@
+package com.example.HK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkApplication.class, args);
+	}
+}
